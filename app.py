75806935906290886c49733215ae38901d417c35
Wbunker <thewillbunker@gmail.com>
2023-12-02 16:29:41 -0600

    First commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/main')
+def main():
+    return 'Important stuff!'
+
+
+@app.route('/health')
+def health_check():
+    return 'Fit as a horse!'
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
